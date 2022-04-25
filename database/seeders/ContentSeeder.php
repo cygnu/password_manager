58efcae226a9193aa729199c5c8391570d5466f0
cygnu <crucia8@icloud.com>
2022-04-25 16:20:24 +0900

    fix:  Using the factory model with seeder

--- database/seeders/ContentSeeder.php
+++ database/seeders/ContentSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Content;
 
 class ContentSeeder extends Seeder
 {
@@ -14,6 +15,6 @@ class ContentSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Content::factory()->count(20)->create();
     }
 }
