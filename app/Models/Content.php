81361c3d9a7d0d7cb0e5b06798e9c8c5c8d51868
cygnu <crucia8@icloud.com>
2022-05-07 16:58:41 +0900

    feat: Create whitelist in app/Models/Content.php

--- app/Models/Content.php
+++ app/Models/Content.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Content extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'content_name',
+        'content_image',
+        'content_url',
+        'is_one_account',
+        'is_paid_subscription',
+    ];
+
+    // protected $casts = [
+    //     'is_one_account' => 'boolean',
+    //     'is_paid_subscription' => 'boolean',
+    // ];
 }
