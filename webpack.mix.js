4d8152a4e5e2d33f48354f00f460a0211b6f4434
cygnu <crucia8@icloud.com>
2022-05-11 03:03:03 +0900

    feat: Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,7 @@ const mix = require('laravel-mix');
 
 mix.ts('resources/ts/index.tsx', 'public/js')
     .postCss('resources/css/app.css', 'public/css');
+
+if (mix.inProduction()) {
+    mix.version();
+}
