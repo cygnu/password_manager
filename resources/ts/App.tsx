d5e4f63854c505a8b1669e3a438d5cb5e4e3c16d
cygnu <crucia8@icloud.com>
2022-05-15 03:12:32 +0900

    feat: Introduce reset css in App.tsx

--- resources/ts/App.tsx
+++ resources/ts/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import emotionReset from 'emotion-reset'
+import { Global, css } from '@emotion/react'
 import { Router } from './router'
 
 export const App = () => {
     return (
-        <Router />
+        <React.Fragment>
+            <Global
+            styles={css`
+                ${emotionReset}
+                li {
+                    list-style: none;
+                }
+            `}
+            />
+            <Router />
+        </React.Fragment>
     )
 }
