a81806248b477cf0a4b25a35f6c8d649f6ee14bb
cygnu <crucia8@icloud.com>
2022-05-15 14:29:33 +0900

    feat: Implement css resources/ts/pages/content/index.tsx

--- resources/ts/pages/content/index.tsx
+++ resources/ts/pages/content/index.tsx
@@ -1,3 +1,4 @@
+/** @jsxImportSource @emotion/react */
 import React from 'react'
 import { Image } from '@mui/icons-material'
 import {
@@ -6,26 +7,52 @@ import {
     CardContent,
     Typography,
 } from '@mui/material'
+import { css } from '@emotion/react'
 
 export const ContentsPage = () => {
     return (
-        <React.Fragment>
-            <Box sx={{ border: '1px solid', maxWidth: 800 }}>
-                <CardContent>
-                    <Image />
-                    <Button>
+        <div css={container}>
+            <Box css={containerBox}>
+                <CardContent css={cBContent}>
+                    <Image
+                        css={cBCImage}
+                    />
+                    <Button css={cBCButton}>
                         <Typography>Amazon</Typography>
                     </Button>
-                    <Typography>https://amazon.com</Typography>
+                    <Typography css={cBCTypography}>https://amazon.com</Typography>
                 </CardContent>
-                <CardContent>
-                    <Image />
-                    <Button>
-                        <Typography>Apple</Typography>
+                <CardContent css={cBContent}>
+                    <Image
+                        css={cBCImage}
+                    />
+                    <Button css={cBCButton}>
+                        <Typography>Amazon</Typography>
                     </Button>
-                    <Typography>https://apple.com</Typography>
+                    <Typography css={cBCTypography}>https://amazon.com</Typography>
                 </CardContent>
             </Box>
-        </React.Fragment>
+        </div>
     )
 }
+
+const container = css``
+
+const containerBox = css`
+    border: 1px solid;
+`
+
+const cBContent = css`
+    display: flex;
+    align-items: center;
+    height: 60px;
+    padding: 0;
+`
+
+const cBCImage = css`
+    font_size: 60px;
+`
+
+const cBCButton = css``
+
+const cBCTypography = css``
