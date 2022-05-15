0e6106ba6c6576811f83923594df48e489e35b41
cygnu <crucia8@icloud.com>
2022-05-15 16:24:51 +0900

    feat: Remove unused components in resource/ts/router.tsx

--- resources/ts/router.tsx
+++ resources/ts/router.tsx
@@ -5,26 +5,11 @@ import { ContentsPage } from './pages/content'
 export const Router = () => {
     return (
         <BrowserRouter>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">ContentPage</Link>
-                        </li>
-                        <li>
-                            <Link to="/about">About</Link>
-                        </li>
-                        <li>
-                            <Link to="/users">Users</Link>
-                        </li>
-                    </ul>
-                </nav>
-                <Routes>
-                    <Route path="/about" element={<About />} />
-                    <Route path="/users" element={<Users />} />
-                    <Route path="/" element={<ContentsPage />} />
-                </Routes>
-            </div>
+            <Routes>
+                <Route path="/about" element={<About />} />
+                <Route path="/users" element={<Users />} />
+                <Route path="/" element={<ContentsPage />} />
+            </Routes>
         </BrowserRouter>
     )
 }
