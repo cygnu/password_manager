27faa805622afbf0cfb9ff929fe35dab46420cad
cygnu <crucia8@icloud.com>
2022-05-31 18:35:01 +0900

    feat: Modify the definition method of factory and generate dummy data with faker

--- database/factories/AccountFactory.php
+++ database/factories/AccountFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Content;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,15 @@ class AccountFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'account_id' => \Ramsey\Uuid\Uuid::uuid4()->toString(),
+            'account_name' => $this->faker->company(),
+            'content_id' => Content::factory(),
+            'email_address' => $this->faker->unique()->safeEmail(),
+            'password' => $this->faker->name(),
+            'is_multi_factor_authentication' => $this->faker->boolean(10),
+            'is_use_oauth2' => $this->faker->boolean(15),
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
     }
 }
