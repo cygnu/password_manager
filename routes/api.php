65b5a591e381034f0511c6748fc35b0b336edb15
cygnu <crucia8@icloud.com>
2022-06-03 04:29:42 +0900

    feat: Add apiResource in routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AccountController;
 use App\Http\Controllers\ContentController;
 
 /*
@@ -16,6 +17,8 @@ use App\Http\Controllers\ContentController;
 */
 
 Route::apiResource('contents', ContentController::class);
+
+Route::apiResource('accounts', AccountController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
