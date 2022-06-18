699b23a0c7e2c425d7a87ad15731b95ad65616b1
cygnu <crucia8@icloud.com>
2022-06-19 04:51:03 +0900

    test: Implement test_update_content() method

--- tests/Feature/ContentTest.php
+++ tests/Feature/ContentTest.php
@@ -46,6 +46,27 @@ class ContentTest extends TestCase
             "is_paid_subscription" => false
         ]);
 
-        $response->assertStatus(201);
+        $response->assertStatus(201)->assertJson([
+            'create' => true
+        ]);
+    }
+
+    /**
+     * @return void
+     * @test
+     */
+    public function update_content()
+    {
+        $response = $this->put('/api/contents/1', [
+            "content_name" => "bar",
+            "content_image" => "https://via.placeholder.com/640x480.png/004400?text=perspiciatis",
+            "content_url" => "http://www.denesik.net/aut-ut-blanditiis-occaecati-et.html",
+            "is_one_account" => false,
+            "is_paid_subscription" => true
+        ]);
+
+        $response->assertStatus(201)->assertJson([
+            'create' => true
+        ]);;
     }
 }
