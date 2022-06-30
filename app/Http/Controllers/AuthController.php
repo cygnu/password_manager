7b7195cc70afd8347ab3098bfa9fb35dee7e0f89
cygnu <crucia8@icloud.com>
2022-06-30 22:14:05 +0900

    feat: Create logout() method in app/Http/Controllers/AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -27,4 +27,21 @@ class AuthController extends Controller
 
         return response()->json([], 401);
     }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return response()->json(true);
+    }
 }
