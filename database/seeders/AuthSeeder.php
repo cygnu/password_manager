45d11d14ef34446be901f3b5a9301fc84c3271e6
cygnu <crucia8@icloud.com>
2022-07-03 16:49:02 +0900

    fix: Create dummy users data

---
+++ database/seeders/AuthSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AuthSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \DB::table('users')->insert([
+            [
+                'name' => 'admin',
+                'email' => 'admin@example.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('123456'),
+                'created_at' => now(),
+                'updated_at' => now()
+            ],[
+                'name' => 'sample',
+                'email' => 'sample@example.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('123456'),
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+        ]);
+    }
+}
