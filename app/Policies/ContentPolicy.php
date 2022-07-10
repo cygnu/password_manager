4ea45693f8ef290a3e7cb283ac3c2b56285eb5cf
cygnu <crucia8@icloud.com>
2022-07-10 19:21:44 +0900

    feat: Update content policy in app/Policies/ContentPolicy.php

--- app/Policies/ContentPolicy.php
+++ app/Policies/ContentPolicy.php
@@ -2,7 +2,7 @@
 
 namespace App\Policies;
 
-use App\Models\Model\Content;
+use App\Models\Content;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
@@ -10,85 +10,10 @@ class ContentPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view any models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function viewAny(User $user)
+    public function checkUser(User $user, Content $content)
     {
-        //
-    }
-
-    /**
-     * Determine whether the user can view the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Model\Content  $content
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function view(User $user, Content $content)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function create(User $user)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Model\Content  $content
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function update(User $user, Content $content)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Model\Content  $content
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function delete(User $user, Content $content)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Model\Content  $content
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function restore(User $user, Content $content)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Model\Content  $content
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function forceDelete(User $user, Content $content)
-    {
-        //
+        if ($user->id === $content->user_id) {
+            return true;
+        }
     }
 }
