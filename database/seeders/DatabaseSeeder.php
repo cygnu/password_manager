86f043c8e939c2737f6cecdd8648af9add401b6d
cygnu <crucia8@icloud.com>
2022-07-10 22:52:24 +0900

    fix: Rename from AuthSeeder to UserSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(AuthSeeder::class);
+        $this->call(UserSeeder::class);
         $this->call(AccountSeeder::class);
     }
 }
