0ae537984a862a97607b3bcbdd8844bf2fdc324f
cygnu <crucia8@icloud.com>
2022-07-13 23:06:21 +0900

    feat: Implement useSignIn and useSignOut in resources/ts/queries/AuthQuery.ts

--- resources/ts/queries/AuthQuery.ts
+++ resources/ts/queries/AuthQuery.ts
@@ -1,5 +1,31 @@
 import * as api from '../api/AuthAPI'
+import { useMutation, useQuery } from 'react-query'
+import { toast } from 'react-toastify'
 
 const useUser = () => {
-  return useQuery('users', api.getUser())
+  return useQuery('users', api.getUser)
 }
+
+const useSignIn = () => {
+  return useMutation(api.signIn, {
+    onSuccess: (user) => {
+      console.log(user)
+    },
+    onError: () => {
+      toast.error('Login failed.')
+    }
+  })
+}
+
+const useSignOut = () => {
+  return useMutation(api.signOut, {
+    onSuccess: (user) => {
+      console.log(user)
+    },
+    onError: () => {
+      toast.error('Logout failed.')
+    }
+  })
+}
+
+export { useUser, useSignIn, useSignOut }
