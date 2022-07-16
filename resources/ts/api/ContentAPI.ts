9f3b440b3a987a2e0fab3e26466b988d9be4b731
cygnu <crucia8@icloud.com>
2022-07-17 01:21:53 +0900

    feat: Pagination implemented

--- resources/ts/api/ContentAPI.ts
+++ resources/ts/api/ContentAPI.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { Content } from '../types/Content'
 
 const getContents = async () => {
-  const { data } = await axios.get<Content[]>('api/contents')
+  const data: { data: Array<Content> } = await axios.get<Array<Content>>('api/contents')
   return data
 }
 
--- resources/ts/pages/content/index.tsx
+++ resources/ts/pages/content/index.tsx
@@ -1,18 +1,35 @@
 /** @jsxImportSource @emotion/react */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ReactPaginate from 'react-paginate'
 import { Image } from '@mui/icons-material'
 import { Box, Button, CardContent, Typography } from '@mui/material'
 import { css } from '@emotion/react'
 import { useContents } from '../../queries/ContentQuery'
+import { Content } from '../../types/Content'
 
 export const ContentsPage: React.FC = () => {
   const { data: contents, status } = useContents()
+  const [postsPerPage] = useState(20)
+  const [pageCount, setPageCount] = useState(0)
+  const [currentPage, setPage] = useState(1)
+
+  useEffect(() => {
+    if (contents) {
+      setPageCount(Math.ceil(contents.data.length / postsPerPage))
+    }
+  }, [currentPage])
+
+  const handlePageClick = (selectedPage: { selected: number }) => {
+    // selectedPage.selectedには、ページ番号 - 1が入る
+    const page = selectedPage.selected + 1
+    setPage(page)
+  }
 
   if (status === 'loading') {
     return <div className="loader" />
   } else if (status === 'error') {
     return <div>Failed to load data.</div>
-  } else if (!contents || contents.length <= 0) {
+  } else if (!contents!.data || contents!.data.length <= 0) {
     return <div>There is no data to display.</div>
   }
 
@@ -20,7 +37,7 @@ export const ContentsPage: React.FC = () => {
     <div css={container}>
       <Box css={containerBox}>
         <CardContent css={cBContent}>
-          {contents.map((content) => (
+          {contents!.data.map((content: Content) => (
             <li key={content.content_id} css={cBCLi}>
               <Image css={cBCImage} />
               <Button css={cBCButton}>
@@ -29,6 +46,15 @@ export const ContentsPage: React.FC = () => {
               <Typography css={cBCTypography}>{content.content_url}</Typography>
             </li>
           ))}
+          <ReactPaginate
+            pageCount={pageCount}
+            onPageChange={handlePageClick}
+            previousLabel={'Previous'}
+            nextLabel={'Next'}
+            breakLabel={'...'}
+            containerClassName={'pagination'}
+            activeClassName={'active'}
+          />
         </CardContent>
       </Box>
     </div>
