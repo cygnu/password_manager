4d4efac9e629f5a36852f581ad8511aaa4ecf651
cygnu <crucia8@icloud.com>
2022-07-23 01:00:58 +0900

    fix: Update createContent

--- resources/ts/api/ContentAPI.ts
+++ resources/ts/api/ContentAPI.ts
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import axios from 'axios'
 import { Content } from '../types/Content'
 
@@ -7,9 +6,25 @@ const getContents = async () => {
   return data
 }
 
-const createContent = async (content_name: string) => {
+const createContent = async ({
+  content_name,
+  content_image,
+  content_url,
+  is_one_account,
+  is_paid_subscription
+}: {
+  content_name: string
+  content_image: string | undefined
+  content_url: string
+  is_one_account: boolean
+  is_paid_subscription: boolean
+}) => {
   const { data } = await axios.post<Content>('api/contents', {
-    content_name: content_name
+    content_name: content_name,
+    content_image: content_image,
+    content_url: content_url,
+    is_one_account: is_one_account,
+    is_paid_subscription: is_paid_subscription
   })
   return data
 }
