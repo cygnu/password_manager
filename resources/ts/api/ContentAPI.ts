ed415b58517ebe5473dc9a40ff7e1050620e2516
cygnu <crucia8@icloud.com>
2022-07-23 00:49:49 +0900

    feat: Add API in resources/ts/api/ContentAPI.ts

--- resources/ts/api/ContentAPI.ts
+++ resources/ts/api/ContentAPI.ts
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import axios from 'axios'
 import { Content } from '../types/Content'
 
@@ -13,4 +14,19 @@ const createContent = async (content_name: string) => {
   return data
 }
 
-export { getContents, createContent }
+const getContent = async (content_id: number) => {
+  const { data } = await axios.get(`api/content/${content_id}`)
+  return data
+}
+
+const updateContent = async ({ content_id, content }: { content_id: number; content: Content }) => {
+  const { data } = await axios.put<Content>(`api/content/${content_id}`, content)
+  return data
+}
+
+const deleteContent = async (content_id: number) => {
+  const { data } = await axios.delete<Content>(`api/content/${content_id}`)
+  return data
+}
+
+export { getContents, createContent, getContent, updateContent, deleteContent }
