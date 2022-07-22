15d28f96fa061950e53c0529de898cd6d8abaeef
cygnu <crucia8@icloud.com>
2022-07-22 23:27:28 +0900

    feat: Add ContentQuery and Fix ContentAPI

--- resources/ts/api/ContentAPI.ts
+++ resources/ts/api/ContentAPI.ts
@@ -1,23 +1,14 @@
 import axios from 'axios'
+import { Content } from '../types/Content'
 
 const getContents = async () => {
   const { data } = await axios.get('api/contents')
   return data
 }
 
-const createContent = async (
-  content_name: string,
-  content_image: string,
-  content_url: string,
-  is_one_account: boolean,
-  is_paid_subscription: boolean
-) => {
-  const { data } = await axios.post('api/contents', {
-    content_name: content_name,
-    content_image: content_image,
-    content_url: content_url,
-    is_one_account: is_one_account,
-    is_paid_subscription: is_paid_subscription
+const createContent = async (content_name: string) => {
+  const { data } = await axios.post<Content>('api/contents', {
+    content_name: content_name
   })
   return data
 }
--- resources/ts/queries/ContentQuery.ts
+++ resources/ts/queries/ContentQuery.ts
@@ -1,8 +1,23 @@
-import { useQuery } from 'react-query'
+import { useMutation, useQuery, useQueryClient } from 'react-query'
+import { toast } from 'react-toastify'
 import * as api from '../api/ContentAPI'
 
 const useContents = () => {
   return useQuery('contents', () => api.getContents())
 }
 
-export { useContents }
+const useCreateContent = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation(api.createContent, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('contents')
+      toast.success('Successfully registered!')
+    },
+    onError: () => {
+      toast.error('Registration failed.')
+    }
+  })
+}
+
+export { useContents, useCreateContent }
