8222a5022ead07912e6f49ae1be35b51078b328a
cygnu <crucia8@icloud.com>
2022-07-23 00:50:37 +0900

    fix: Add undefined to content_image

--- resources/ts/types/Content.ts
+++ resources/ts/types/Content.ts
@@ -1,7 +1,7 @@
 type Content = {
-  content_id: string
+  content_id: number
   content_name: string
-  content_image: string
+  content_image: string | undefined
   content_url: string
   is_one_account: boolean
   is_paid_subscription: boolean
