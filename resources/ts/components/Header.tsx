c51d7e2db0e24218b5c966292187cbd68b75183e
cygnu <crucia8@icloud.com>
2022-07-27 00:01:08 +0900

    feat: Implemented so that the header changes in the login state.

--- resources/ts/components/Header.tsx
+++ resources/ts/components/Header.tsx
@@ -1,17 +1,32 @@
 /** @jsxImportSource @emotion/react */
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { AppBar, Toolbar, Typography, Button } from '@mui/material'
 import { css } from '@emotion/react'
+import { useAuth } from '../hooks/AuthContext'
+import { useUser } from '../queries/AuthQuery'
 
 export const Header: React.FC = () => {
+  const { isAuth, setIsAuth } = useAuth()
+  const { isLoading, data: authUser } = useUser()
+
+  useEffect(() => {
+    if (authUser) {
+      setIsAuth(true)
+    }
+  }, [authUser])
+
   return (
     <AppBar position="static" css={appBar}>
       <Toolbar>
         <Typography variant="h6">Password Manager</Typography>
-        <Button variant="outlined" size="small" component={Link} to="/login" color="inherit">
-          Login
-        </Button>
+        {isAuth ? (
+          <Typography>{authUser?.email}</Typography>
+        ) : (
+          <Button variant="outlined" size="small" component={Link} to="/login" color="inherit">
+            Login
+          </Button>
+        )}
       </Toolbar>
     </AppBar>
   )
